ba8cb84c8213130677b9b61fcb58f6d8869f7ab7
Martijn Coenen <maco@google.com>
2018-05-11 01:45:24 -0700

    UPSTREAM: ANDROID: binder: remove 32-bit binder interface.
    New devices launching with Android P need to use the 64-bit
    binder interface, even on 32-bit SoCs [0].
    
    This change removes the Kconfig option to select the 32-bit
    binder interface. We don't think this will affect existing
    userspace for the following reasons:
    1) The latest Android common tree is 4.14, so we don't
       believe any Android devices are on kernels >4.14.
    2) Android devices launch on an LTS release and stick with
       it, so we wouldn't expect devices running on <= 4.14 now
       to upgrade to 4.17 or later. But even if they did, they'd
       rebuild the world (kernel + userspace) anyway.
    3) Other userspaces like 'anbox' are already using the
       64-bit interface.
    
    Note that this change doesn't remove the 32-bit UAPI
    itself; the reason for that is that Android userspace
    always uses the latest UAPI headers from upstream, and
    userspace retains 32-bit support for devices that are
    upgrading. This will be removed as well in 2-3 years,
    at which point we can remove the code from the UAPI
    as well.
    
    Finally, this change introduces build errors on archs where
    64-bit get_user/put_user is not supported, so make binder
    unavailable on m68k (which wouldn't want it anyway).
    
    [0]: https://android-review.googlesource.com/c/platform/build/+/595193
    
    Signed-off-by: Martijn Coenen <maco@android.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 1190b4e38f97023154e6b3bef61b251aa5f970d0)
    
    Change-Id: I73dadf1d7b45a42bb18be5d5d3f5c090e61866de
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -73,10 +73,6 @@
 
 #include <linux/proc_fs.h>
 
-#ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
-#define BINDER_IPC_32BIT 1
-#endif
-
 #include <uapi/linux/android/binder.h>
 #include "binder_alloc.h"
 #include "binder_trace.h"
