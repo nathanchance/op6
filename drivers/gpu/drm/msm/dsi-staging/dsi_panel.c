22df523e2a827b73bdeb68e72eb193ab4a5cb68a
Nathan Chancellor <natechancellor@gmail.com>
2018-05-19 17:27:50 -0700

    drm/msm/dsi-staging: Fix indentation in dsi_panel_update_backlight
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -697,10 +697,10 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 
 	dsi = &panel->mipi_device;
 
-    if (panel->bl_config.bl_high2bit){
-        rc = mipi_dsi_dcs_set_display_brightness_samsung(dsi, bl_lvl);
-    } else
-	rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
+	if (panel->bl_config.bl_high2bit)
+		rc = mipi_dsi_dcs_set_display_brightness_samsung(dsi, bl_lvl);
+	else
+		rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
 	if (rc < 0)
 		pr_err("failed to update dcs backlight:%d\n", bl_lvl);
 
